1dda4422c110c0371153c6307e360027f2e430c8
W.G.R Hasaranga <106307238+hasaranga99@users.noreply.github.com>
2023-08-28 22:50:03 +0530

    Jwt Authorized

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 
 function Home() {
-  return <div>HomePage</div>;
+
+  const [auth,setAuth] = useState(false)
+  console.log('check auth', auth)
+  const [name,setName] = useState('')
+  const [message, setMessage] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:8081')
+    .then(res => {
+      // console.log('check api',res)
+      if(res.data.Status === "Success"){
+        setAuth(true);
+        setName(res.data.name);
+       
+
+
+      }else{
+        setAuth(false);
+        setMessage(res.data.Message);
+      }
+    })
+  },[])
+
+  const handleLogout = () =>{
+    axios.get('http://localhost:8081/logout')
+  }
+
+  return (
+    <div className="container mt-4">
+      {auth ? (
+        <div>
+          <h3>You are Authorized {name}</h3>
+          <button className="btn btn-danger" onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h3>{message}</h3>
+          <h3>Login Now</h3>
+          <button className="btn btn-primary">Login</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Home;
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useNavigate } from "react-router-dom";
 
 
-function Login() {
+export default function Login() {
+
+  const[values, setValues] = useState({
+    email:'',
+    password:''
+  })
+
+  const navigate = useNavigate()
+
+  axios.defaults.withCredentials = true;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:8081/login', values)
+    .then(res => {
+      if(res.data.Status === "Success"){
+        navigate('/')
+      }else {
+        alert(res.data.Message)
+      }
+    })
+    .catch(err => console.log(err));
+    
+
+  }
+
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-50">
         <h2>Sign-In</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
         <label htmlFor="email"><strong>Email</strong></label>
-        <input type="email" placeholder="Enter Email" name="email" autoComplete="off" className="form-control rounded-0"></input>
+        <input type="email" placeholder="Enter Email" name="email" autoComplete="off" onChange={e => setValues({...values, email: e.target.value})} className="form-control rounded-0"></input>
         </div>
         <div className="mb-3">
         <label htmlFor="password"><strong>Password</strong></label>
-        <input type="password" placeholder="Enter Password" name="password" autoComplete="off" className="form-control rounded-0"></input>
+        <input type="password" placeholder="Enter Password" name="password" autoComplete="off" onChange={e=> setValues({...values,password:e.target.value})} className="form-control rounded-0"></input>
         </div>
         <button type="submit" className="btn btn-success w-100 rounded-0">Log in</button>
         <p className="d-flex justify-content-center align-items-center ">You are agree to our terms and policies</p>
@@ -25,4 +51,4 @@ function Login() {
   );
 }
 
-export default Login;
+
